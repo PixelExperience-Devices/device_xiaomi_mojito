57dd182ad76bf9a4844cc3f8af014db43168ee67
Pig <pig.priv@gmail.com>
2020-09-19 00:09:28 +0800

    mojito: lights: Extend hidl changes to aidl
     * Dianlujitao for authoring commits 7174356e24cdf8f7aa639475be287c18a139ea49, and f8e30fd8fb7ab7d12a9308a04771008ff2a924c5.
    
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I09c5f7222d29320bd43e46b6f55e0733092b02d4

--- lights/Lights.h
+++ lights/Lights.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +21,8 @@
 #include <hardware/hardware.h>
 #include <hardware/lights.h>
 #include <map>
+#include <sstream>
+#include <unordered_map>
 
 namespace aidl {
 namespace android {
@@ -27,15 +30,31 @@ namespace hardware {
 namespace light {
 
 class Lights : public BnLights {
-    public:
-      Lights();
-      ndk::ScopedAStatus setLightState(int id, const HwLightState& state) override;
-      ndk::ScopedAStatus getLights(std::vector<HwLight>* types) override;
+  public:
+    Lights();
+    ndk::ScopedAStatus setLightState(int id, const HwLightState& state) override;
+    ndk::ScopedAStatus getLights(std::vector<HwLight>* types) override;
 
-    private:
-      std::map<int, light_device_t*> mLights;
-      std::vector<HwLight> mAvailableLights;
-      int maxLights;
+  private:
+    void setLightBacklight(LightType type, const HwLightState& state);
+    void setLightNotification(LightType type, const HwLightState& state);
+    void applyNotificationState(const HwLightState& state);
+
+    uint32_t max_led_brightness_;
+    uint32_t max_screen_brightness_;
+
+    std::unordered_map<LightType, std::function<void(LightType type, const HwLightState&)>> lights_{
+            {LightType::ATTENTION, [this](auto&&... args) { setLightNotification(args...); }},
+            {LightType::BACKLIGHT, [this](auto&&... args) { setLightBacklight(args...); }},
+            {LightType::BATTERY, [this](auto&&... args) { setLightNotification(args...); }},
+            {LightType::NOTIFICATIONS, [this](auto&&... args) { setLightNotification(args...); }}};
+
+    // Keep sorted in the order of importance.
+    std::array<std::pair<LightType, HwLightState>, 3> notif_states_ = {{
+            {LightType::ATTENTION, {}},
+            {LightType::NOTIFICATIONS, {}},
+            {LightType::BATTERY, {}},
+    }};
 };
 
 }  // namespace light
