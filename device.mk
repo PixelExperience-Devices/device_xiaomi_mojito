646d9bc8e2ec5627e07f221b415d12b970234113
TheMalachite <eliasgheeraert@gmail.com>
2021-03-30 16:19:13 +0200

    mojito: keylayout: Import keylayout from stock
    Change-Id: Id7e0507e4e2aa45d71353f54f0a2e3c2c5394e0d

--- device.mk
+++ device.mk
@@ -116,6 +116,12 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.sh \
     init.recovery.qcom.rc
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # Kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/kernel:kernel \
