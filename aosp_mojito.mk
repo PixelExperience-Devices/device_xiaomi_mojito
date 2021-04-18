410fa3c4b8115be5d6ae85f956ca0af31078633d
Zinadin Zidan <zidan44@pixelexperience.org>
2021-04-18 13:45:38 +0600

    mojito: Disable pixel live wallpapers
    * As it makes lags.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I113fb761f0c0662d54bf5083c752e25a0d8b4856

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/mojito/device.mk)
 # Inherit some common PixelExperience stuff
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
