8f73c2c2219d7d2835aa2f01a26b12886a109df5
Bruno Martins <bgcngm@gmail.com>
2021-04-20 21:56:48 +0100

    sm6150-common: Build power-off charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5

--- sm6150.mk
+++ sm6150.mk
@@ -151,6 +151,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
