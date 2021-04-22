6bcd9d5831457c295a96c97a96f95ab21a93148d
Zinadin Zidan <zidan44@pixelexperience.org>
2021-04-22 13:01:18 +0600

    mojito: Switch to source build kernel
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I0b59d27eafd054183e7716db72bf99495e5484d8

--- device.mk
+++ device.mk
@@ -129,11 +129,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
-# Kernel
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/kernel:kernel \
-    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.qti
