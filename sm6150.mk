aa2fbf1a2c9a2497062dd02f100e89cb5874e14b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:58:06 +0200

    sm6150-common: Import excluded-input-devices.xml from stock
    Change-Id: I6f384c556e124052b3695a87f225207dae7c8089

--- sm6150.mk
+++ sm6150.mk
@@ -315,6 +315,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_sm6150
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
