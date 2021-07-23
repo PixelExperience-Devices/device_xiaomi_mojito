3e424b1becc3f23bd46bdd2396bed99e2d5f9a71
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-24 01:28:42 +0600

    sm6150-common: Build usb service packages
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Id2b0a5d59137367aa8b8be4373de03009047a417

--- sm6150.mk
+++ sm6150.mk
@@ -333,6 +333,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
