39b0ee3dc87fdd56461a06e1eb0a8f6af4b422b4
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 19:19:15 +0600

    sm6150-common: Build drm clearkey packages
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I9666d5ba4679bbaf173fd3450450cc5403edfebb

--- sm6150.mk
+++ sm6150.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3-service.clearkey
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
