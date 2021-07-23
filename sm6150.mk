b31544600984fc22b6715b9390f1099d47b87e1d
Ethan Chen <intervigil@gmail.com>
2021-07-23 15:57:24 +0600

    sm6150-common: Setup local soong namespace
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I541d31009bf134ec30198b44ae5a3800d1f3ce7f

--- sm6150.mk
+++ sm6150.mk
@@ -22,3 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
