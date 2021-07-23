8bdb920dfba0773cd8b19ad128a562d241fa0fc3
Christian Oder <myself5@carbonrom.org>
2021-07-23 14:59:07 +0600

    sm6150-common: Build disabled VBMeta image
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ief5d6b965db91391ddd3042edfd688160f27869c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,3 +89,7 @@ TARGET_USES_MKE2FS := true
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
