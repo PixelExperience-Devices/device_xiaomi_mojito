965cdc1ae1888286c004ebfeff55ab35b87a1be9
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 16:33:28 +0600

    sm6150-common: Build required audio hals
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I8ea05ccdddd9ac54d18113182fb0bef5a10f8e85

--- sm6150.mk
+++ sm6150.mk
@@ -36,6 +36,36 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.common@2.0-util \
+    android.hardware.audio.common@4.0-util \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libaudio-resampler \
+    libvolumelistener \
+    libtinycompress \
+    libtinycompress.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
