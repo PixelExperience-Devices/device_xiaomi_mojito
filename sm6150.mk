0ad67aac53c32e52284a4ddac9abfea81f81377d
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 20:10:28 +0600

    sm6150-common: Build perf interface libs
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ida97e2d84b4b02a14560316b3cc5821db8a5b432

--- sm6150.mk
+++ sm6150.mk
@@ -245,6 +245,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
