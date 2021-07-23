4c6b250ae277b447b2304dee175d6e66f43765ec
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 19:21:35 +0600

    sm6150-common: Build qcom FM Radio packages
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I112eac56684341b27ca01606864e248a7d6ac12d

--- sm6150.mk
+++ sm6150.mk
@@ -150,6 +150,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    qcom.fmradio
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
