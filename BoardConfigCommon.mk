d9215c1ec0de4e0966a43c2576d7d7e21652dc0a
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 14:37:25 +0600

    sm6150-common: Add arch and bootloader flags
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I462d0552e24730d181be15eca2d1affa2c06a5f0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,3 +20,22 @@ COMMON_PATH := device/xiaomi/sm6150-common
 
 # Inherit from proprietary files
 include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_NO_BOOTLOADER := true
