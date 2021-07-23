231feaac33325eed9aa6429d253c32774f3de880
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 20:08:54 +0600

    sm6150-common: Copy up to date software.ipsec_tunnels to system
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I81a79edd5bc9859cd871e500424c3a81c915db14

--- sm6150.mk
+++ sm6150.mk
@@ -241,6 +241,10 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Network
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
