355765babf86718fffb298f5f0a27b9030b1af16
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 15:41:12 +0600

    sm6150-common: Add a minimal framework compatibility matrix
    * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I2a81bba54fc5d55aed2310e07eec07e44df649a4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
