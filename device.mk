6abfa1acffa4da1ae3bebd67d7f2a5a0b75f030e
daniml3 <danimoral1001@gmail.com>
2021-07-25 23:24:40 +0200

    mojito: cutout: Show a ring on the camera cutout when its in use
    * For avoiding halo effect on low light conditions
    
    * Achieve this by creating a custom ImageView that its shown when the
      camera is in use
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Iedca0f0d4b304b020daf0ebec74e245581824c98
    Signed-off-by: Prince Cooper <pcooper7736@gmail.com>

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Cutout Ring
+PRODUCT_PACKAGES += \
+    CutoutRingServiceMojito
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
