515a1d2a4ecbf80ffcb7c613a4ef89c831c5c2bd
Abhishek Aggarwal <warabhishek@gmail.com>
2021-07-25 23:48:27 +0600

    sm6150-common: keylayout: Import headset jack layout
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: If7ad0190ae4ccdd5dbdeb216fbb57085b87d0b53

--- sm6150.mk
+++ sm6150.mk
@@ -210,7 +210,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
+    $(LOCAL_PATH)/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
 
 # Lights
 PRODUCT_PACKAGES += \
