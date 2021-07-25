f65fdd3db3b70090b269b74f73d97b5984dcd720
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-25 23:32:40 +0600

    sm6150-common: Add fpc and goodix keylayouts
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I67890090f68a5173b1ca8b5796b981bf04903189

--- sm6150.mk
+++ sm6150.mk
@@ -206,6 +206,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sm6150
