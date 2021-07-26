ddba79fb047241831fcb2ea7844184959e307f4b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-26 07:18:11 +0600

    sm6150-common: libinit: Initial variant handler libinit
    * This libinit has been made to commonize device variants props handling
    
    Change-Id: I8f8aaf5c10d31ac3e1e4526f0eb94b676c182c46

---
+++ libinit/include/libinit_sm6150.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIBINIT_SM6150_H
+#define LIBINIT_SM6150_H
+
+#include <string>
+#include <vector>
+
+typedef struct variant_info {
+    std::string hwc_value;
+    std::string sku_value;
+
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+
+    bool nfc;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+void set_variant_props(const variant_info_t variant);
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);
+
+#endif // LIBINIT_SM6150_H
