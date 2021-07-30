c5bb44ba9c5ae3c642af920ad8a22ec8de81df7c
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-31 00:16:52 +0600

    sm6150-common: Build sensors service hals
    * Also guard multihal sensors
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I2b00a00ba017c986a631173771ca8cca8688b5c6

--- sm6150.mk
+++ sm6150.mk
@@ -350,7 +350,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
-# Sensors
+ifeq ($(TARGET_ENABLE_MULTI_SENSOR),true)
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal
+else
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+endif
+
+# Sensors configs
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
