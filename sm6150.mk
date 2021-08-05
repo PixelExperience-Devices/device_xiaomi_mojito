27538359d4ad64e14b0d9ab74030f76e592c3e48
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-05 19:23:48 +0600

    sm6150-common: Build some more camera packages
    * Match with stock.
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I43e2cbb0c2150822ad5b097776133459e36a343b

--- sm6150.mk
+++ sm6150.mk
@@ -106,9 +106,16 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libgui_vendor \
+    libdng_sdk.vendor \
+    libcamera2ndk_vendor \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libdng_sdk.vendor
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.device@2.0.vendor \
+    vendor.qti.hardware.camera.device@3.5.vendor
 
 PRODUCT_PACKAGES += \
     Snap
