71b0a31aac63de9b335fcc81707efee13ac7887b
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-20 14:17:17 +0600

    sm6150-common: gps: Switch to prebuild GNSS stack
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I145ce61d48033ab6f6b17d39706345452287a6ab

--- sm6150.mk
+++ sm6150.mk
@@ -186,20 +186,7 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
-# GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1-impl-qti \
-    android.hardware.gnss@2.1-service-qti
-
-PRODUCT_PACKAGES += \
-    libbatching \
-    libgeofencing \
-    libgnss
-
-# GPS Configs
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/gps/confs/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
+# GPS Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
