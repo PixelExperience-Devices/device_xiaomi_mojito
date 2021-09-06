79eb46330e1d70444c15fc96869e484e0ec2cb73
LuK1337 <priv.luk@gmail.com>
2021-09-06 11:21:06 +0600

    sm6150-common: Include device FCM from vendor/aosp
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,9 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
