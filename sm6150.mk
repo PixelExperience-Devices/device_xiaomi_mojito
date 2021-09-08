82c1cc26d0a173192a84ca06baaf4ac69037bb23
Zinadin Zidan <zidan.roking@gmail.com>
2021-09-09 00:22:52 +0600

    sm6150-common: Guard qti thermal 2.0 service hals
    * Its crashes on mojito but fine on sweet
    
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I56d844402e8ec6d8e0dfd145495cc19619ae7b75

--- sm6150.mk
+++ sm6150.mk
@@ -406,8 +406,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
