c95f1174bcc38ee6a72569ceb1d8283591ba0079
Zinadin Zidan <zidan.roking@gmail.com>
2021-09-09 00:17:49 +0600

    sm6150-common: Use QTI USB init configs from USB HAL repo
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I25a3b2ff79e6c80069ece32bbacf211691495637

--- sm6150.mk
+++ sm6150.mk
@@ -347,7 +347,6 @@ PRODUCT_PACKAGES += \
     init.nfc.rc \
     init.qcom.power.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
@@ -417,6 +416,13 @@ PRODUCT_COPY_FILES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
