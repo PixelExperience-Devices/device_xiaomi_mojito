3e2e683fc42afde19ca4b5d475a5011cbcae1a99
Alexander Koskovich <zvnexus@outlook.com>
2021-09-14 23:17:27 +0600

    sm6150-common: Remove virtual framebuffer kernel cmdline
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I51becacd30d17e71cdeb0fbe31bbe1b94262b316

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,7 +110,6 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CLANG_COMPILE := true
