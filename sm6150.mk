b633707c787f773a59dda8376d0a6c6a770692cb
Zinadin Zidan <zidan.roking@gmail.com>
2021-09-23 18:36:13 +0600

    sm6150-common: Build power stats hardware service mock
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I05e568f4738fdb05e29454f65252fbd2b4798d30

--- sm6150.mk
+++ sm6150.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
