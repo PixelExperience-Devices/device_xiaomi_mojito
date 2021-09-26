b64c2befbd7c6764e54e0d2fe4bebea574d55951
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-26 12:15:15 +0600

    sm6150-common: Kill kernel cmdline console parameters
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I00c5399963ea4c4b70baf97b0abbdf1b0b41ebb4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,12 +89,9 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
-BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
