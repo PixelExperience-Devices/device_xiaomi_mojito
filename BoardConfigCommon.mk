62203df3170ce84afe73736f6601622a81b4a58f
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-08 12:07:09 +0600

    sm6150-common: Use to vendor C2 manifest entry
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ic3424c2c4dc6ad617cc490469093be72492bab81

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,7 +92,7 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest_vendor.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += nfc
