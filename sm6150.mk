51d5e85ee9729f9fa18db92ef4b71c4eac28663b
Anay Wadhera <awadhera@berkeley.edu>
2022-02-08 12:15:49 +0600

    sm6150-common: Build extphonelib libs
    Change-Id: I8dc8d3684d6850d1e4231171fb3b4dcdfb5332e2

--- sm6150.mk
+++ sm6150.mk
@@ -460,6 +460,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
