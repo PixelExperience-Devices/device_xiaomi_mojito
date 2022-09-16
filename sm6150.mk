dfc7884b89aaa235fffa8d4c8537ff09ccf6c97b
Josh Wu <joshwu@google.com>
2022-09-16 19:01:10 +0600

    sm6150-common: Switch to AIDL bluetooth audio
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I34ab5856ae538d200167f461ed513a3ff2aed1b4

--- sm6150.mk
+++ sm6150.mk
@@ -115,7 +115,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
