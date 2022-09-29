3b7af61b6e4a1e97508d8963c102bd3b6252ad9b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 13:28:36 +0600

    mojito: Build default health HIDL recovery impl
    Change-Id: Iaa6b7adcdef73f2485df77cf5df61b34d236eece

--- device.mk
+++ device.mk
@@ -262,7 +262,8 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # IFAA manager
 PRODUCT_PACKAGES += \
