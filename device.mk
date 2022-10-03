c183f829aeb579d491fe62ff0d3e6376ee09d36e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-03 17:58:05 +0600

    mojito: Move to common Xiaomi fingerprint HIDL
    Change-Id: Ib008e6a763f237042d554d8f048799926d2f02b4

--- device.mk
+++ device.mk
@@ -212,7 +212,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
@@ -395,6 +395,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.xiaomi.rc \
     ueventd.qcom.rc
 
 # RIL
