082b03a14c44cfd52bc6ad3d19e97bf2925f6bb4
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-05 21:27:29 +0600

    mojito: Move media configs to proprietary-files
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ic0c0b3dbaa793126da4a766ffb6996c8197c6992

--- device.mk
+++ device.mk
@@ -311,12 +311,6 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
-
-PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
