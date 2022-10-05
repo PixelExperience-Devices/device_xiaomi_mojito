32fa3d8d9ba2e2283af48e5c0ba60a65ea59d36a
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-05 21:44:47 +0600

    mojito: Drop XiaomiParts app
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I5c3aad7cb433ae13070e3ab96985092f1ea64e94

--- device.mk
+++ device.mk
@@ -156,10 +156,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Device Specific Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
