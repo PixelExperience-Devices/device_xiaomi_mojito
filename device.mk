43b1d666c0ac044fe686ffefa591849e43d7bd28
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-06 11:32:43 +0600

    mojito: vibrator: Call vibrator-vendor-product.mk
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I8243285efaf1161616eabb1fa4b916237a8ca67a

--- device.mk
+++ device.mk
@@ -476,11 +476,7 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
