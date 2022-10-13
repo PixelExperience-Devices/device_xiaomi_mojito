6c71bd1c6150e34324265ee2a3ed4b3e855c0745
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-14 00:50:02 +0600

    mojito: Drop xiaomi Mlipay service blobs
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I0204fc2cabdc1e4e9356d6fdfdbcd71722c9244f

--- device.mk
+++ device.mk
@@ -256,13 +256,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
     android.hardware.health@2.1-impl.recovery
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
