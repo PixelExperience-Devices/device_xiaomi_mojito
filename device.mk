eeae5b124073b97cf71605402d48107419228618
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-02-18 03:02:27 +0600

    mojito: Build fm interfaces from source
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ic28049369e4beee750c77dcab4be3570b882b565

--- device.mk
+++ device.mk
@@ -213,6 +213,9 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.fm@1.0.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
